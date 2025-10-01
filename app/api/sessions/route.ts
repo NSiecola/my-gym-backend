ca1cd161b9925977f50aca6e03642d4a0d70b713
Nicholas Siécola <n.siecola@gec.inatel.br>
2025-09-30 21:52:30 -0300

    arrumando a segurança do `sessions`

--- app/api/sessions/route.ts
+++ app/api/sessions/route.ts
@@ -3,32 +3,31 @@ import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 
 export async function POST(request: Request) {
-
     const client = await db.connect();
-
     try {
-        const body = await request.json();
-        const { user_id, routine_name } = body;
+        const headersList = headers();
+        const userId = (await headersList).get('x-user-id');
 
-        if (!user_id) {
-            return NextResponse.json({ message: 'O ID do usuário (user_id) é obrigatório' }, { status: 400 });
+        const body = await request.json();
+        const { routine_name } = body;
+
+        if (!userId) {
+            return NextResponse.json({ message: 'Não autorizado' }, { status: 401 });
         }
 
         const queryText = `
-            INSERT INTO workout_sessions (user_id, routine_name) 
-            VALUES ($1, $2) 
-            RETURNING *; 
+            INSERT INTO workout_sessions (user_id, routine_name)
+            VALUES ($1, $2)
+            RETURNING *;
         `;
 
-        const queryParams = [user_id, routine_name || null];
+        const queryParams = [userId, routine_name || null];
 
         const result = await client.query(queryText, queryParams);
-
         return NextResponse.json(result.rows[0], { status: 201 });
 
     } catch (error) {
         console.error('Erro ao criar sessão de treino:', error);
-
         return NextResponse.json({ message: 'Erro interno do servidor' }, { status: 500 });
     } finally {
         client.release();
